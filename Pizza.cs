15b8058571d92a4dccf71b907dd5ed5e2f4be0b8
Pioterg <piotrsitkowski@wp.pl>
2017-01-09 16:18:26 +0100

    Create Pizza.cs

---
+++ Pizza.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolo3
+{
+    class Pizza
+    {
+        private string nazwa;
+        private decimal suma = 15.0m;
+        private string skladniki;
+        private string sos;
+
+
+
+    }
+}
